6c4e36bfd032661f2dc12c338f558de2e2b5eb7c
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-08-30 15:16:15 -0300

    feat: ajusts in controller in error request

--- src/modules/accounts/useCases/CreateUserController.ts
+++ src/modules/accounts/useCases/CreateUserController.ts
@@ -5,17 +5,21 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 
 class CreateUserController {
   async handle (request: Request, response: Response): Promise<Response> {
-    const { name, email, password, driver_license } = request.body;
-    const createUserUseCase = container.resolve(CreateUserUseCase);
+    try {
+      const { name, email, password, driver_license } = request.body;
+      const createUserUseCase = container.resolve(CreateUserUseCase);
 
-    await createUserUseCase.execute({
-      name,
-      password,
-      email,
-      driver_license
-    });
+      await createUserUseCase.execute({
+        name,
+        password,
+        email,
+        driver_license
+      });
 
-    return response.status(201).send();
+      return response.status(201).send();
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
