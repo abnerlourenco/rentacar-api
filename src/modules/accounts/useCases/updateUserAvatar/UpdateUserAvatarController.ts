ceb9c446d1fc6649ba03f5b634936fa9fdd56423
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-09-06 11:22:16 -0300

    feat: add update user avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,29 @@
+import { type Request, type Response } from 'express';
+import { container } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
+
+class UpdateUserAvatarController {
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
+    // receber arquivo
+    const avatar_file = request.file?.filename;
+
+    if (!avatar_file) {
+      throw new AppError('file not found');
+    }
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    await updateUserAvatarUseCase.execute({
+      user_id: id,
+      avatar_file
+    });
+
+    return response.status(204).send();
+  }
+}
+
+export { UpdateUserAvatarController };
---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string
+  avatar_file: string
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor (
+    @inject('UsersRepository')
+    private readonly usersRepository: IUsersRepository
+  ) {}
+
+  async execute ({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User is not found');
+    }
+
+    // salvar no storage e gerar o link
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
