3d9acf17094094182cbee330d1ef1e2564dadcc7
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2024-11-22 19:24:36 -0300

    feat: added s3 provider to save profile and cars images on AWS

---
+++ src/shared/container/providers/StorageProvider/implementations/AwsS3StorageProvider.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import mime from 'mime';
+import { resolve } from 'path';
+
+import { DeleteObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+import upload from '../../../../../config/upload';
+import { AppError } from '../../../../errors/AppError';
+import { type IStorageProvider } from '../IStorageProvider';
+
+class AwsS3StorageProvider implements IStorageProvider {
+  private readonly client: S3Client;
+
+  constructor () {
+    this.client = new S3Client({
+      region: process.env.AWS_BUCKET_REGION
+    });
+  }
+
+  async save (file: string, folder: string): Promise<string> {
+    const originalName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalName);
+
+    const ContentType = mime.getType(originalName);
+
+    if (ContentType === null) {
+      throw new AppError('Content file type is null');
+    }
+
+    if (process.env.AWS_BUCKET_NAME === undefined) {
+      throw new AppError('AWS_BUCKET_NAME is undefined');
+    }
+
+    await this.client.send(new PutObjectCommand({
+      Bucket: `${process.env.AWS_BUCKET_NAME}`,
+      Key: `${folder}/${file}`,
+      // ACL: 'public-read-write',
+      Body: fileContent,
+      ContentType
+    })
+    );
+
+    await fs.promises.unlink(originalName);
+
+    return file;
+  }
+
+  async delete (file: string, folder: string): Promise<void> {
+    if (process.env.AWS_BUCKET_NAME === undefined) {
+      throw new AppError('AWS_BUCKET_NAME variable is undefined');
+    }
+
+    await this.client.send(new DeleteObjectCommand({
+      Bucket: `${process.env.AWS_BUCKET_NAME}`,
+      Key: `${folder}/${file}`
+    })
+    );
+  }
+}
+
+export { AwsS3StorageProvider };
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,13 @@
 import { container } from 'tsyringe';
 
+import { AppError } from '../../errors/AppError';
 import { type IDateProvider } from './DateProvider/IDateProvider';
 import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
 import { type IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { AwsS3StorageProvider } from './StorageProvider/implementations/AwsS3StorageProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { type IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayJsDateProvider',
@@ -13,4 +17,18 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
   'EtherealMailProvider',
   new EtherealMailProvider()
+);
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: AwsS3StorageProvider
+};
+
+if (process.env.DISK === undefined) {
+  throw new AppError('DISK variable is undefined');
+}
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  diskStorage[process.env.DISK]
 );
